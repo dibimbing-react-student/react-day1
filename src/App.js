7af29c78ba0effec26df6127720b9ac71d3051a1
ihsanuddienullah <ihsanuddienullah@gmail.com>
2022-05-08 14:45:00 +0700

    react fundamental class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Functional Component
+// function App() {
+
+//   let x = 800;
+//   x = 1000;
+
+//   const y = 300;
+
+//   return (
+//     <div className="App">
+//       <h1>Hello React</h1>
+//       <p>{x}</p>
+//       <p>{y}</p>
+//     </div>
+//   );
+// }
+
+// Class Component
+class App extends React.Component {
+    render() {
+        let x = 800;
+        x = 1000;
+
+        const y = 300;
+
+        return (
+            <div className="App">
+                <h1>Hello React</h1>
+                <p>{x}</p>
+                <p>{y}</p>
+            </div>
+        );
+    }
 }
 
 export default App;
